f1364e414087240e6352aa5b48f7b124afe3613d
rishabh <rshvraj36@gmail.com>
2025-08-14 17:29:14 +0530

    feat: Update universal_mcp dependency and enhance SemrushApp class with API key management

--- src/universal_mcp_semrush/app.py
+++ src/universal_mcp_semrush/app.py
@@ -5,19 +5,44 @@ class SemrushApp(APIApplication):
     """
     Base class for Universal MCP Applications.
     """
-    def __init__(self, integration: Integration = None, **kwargs) -> None:
+    def __init__(self, integration: Integration | None = None, **kwargs) -> None:
         super().__init__(name="semrush", integration=integration, **kwargs)
+        self.base_url="https://api.semrush.com"
+        self._api_key: str | None = None
+      
+    def _get_headers(self):
+        if not self.integration:
+            raise ValueError("Integration not found")
+        credentials = self.integration.get_credentials()
+        if "api_key" in credentials:
+            self.api_key = credentials["api_key"]
+        # Always return empty headers
+        return {}
 
-    def run(self):
+    @property
+    def api_key(self):
+        """Gets the API key by calling _get_headers and caches it."""
+        if self._api_key:
+            return self._api_key
+        self._get_headers()
+        return self._api_key
+
+    @api_key.setter
+    def api_key(self, value: str) -> None:
+        """Sets the API key.
+        
+        Args:
+            value (str): The API key to set.
         """
-        Example tool implementation.
-        """
-        print(f"Running the main task for {self.name}...")
-        print("Hello from SemrushApp!")
-        return "Task completed successfully."
+        self._api_key = value
+
+    def domain_add_history(self):
+        pass
+
+   
 
     def list_tools(self):
         """
         Lists the available tools (methods) for this application.
         """
-        return [self.run]
+        return []
